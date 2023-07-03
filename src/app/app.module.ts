7efa649272c8e8a86c83db62ccb9a98cfa37e3bc
Meher Tasnim <mehertasnim95@gmail.com>
2023-07-03 17:11:31 -0500

    added all components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,27 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { ListingsPageComponent } from './listings-page/listings-page.component';
+import { AppRoutingModule } from './app-routing.module';
+import { ListingDetailPageComponent } from './listing-detail-page/listing-detail-page.component';
+import { ContactPageComponent } from './contact-page/contact-page.component';
+import { MyListingsPageComponent } from './my-listings-page/my-listings-page.component';
+import { NewListingsPageComponent } from './new-listings-page/new-listings-page.component';
+import { EditListingPageComponent } from './edit-listing-page/edit-listing-page.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ListingsPageComponent,
+    ListingDetailPageComponent,
+    ContactPageComponent,
+    MyListingsPageComponent,
+    NewListingsPageComponent,
+    EditListingPageComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
